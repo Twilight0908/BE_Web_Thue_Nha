c0277f9612206dca2ba6ce955778ab70cb285eba
Twilight0908 <nguyenquang00.it@gmail.com>
2023-09-06 11:31:34 +0700

    first commit

---
+++ WebThueNha/src/main/java/com/webthuenha/WebThueNhaApplication.java
@@ -0,0 +1,13 @@
+package com.webthuenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThueNhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebThueNhaApplication.class, args);
+	}
+
+}
---
+++ WebThueNha/src/test/java/com/webthuenha/WebThueNhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webthuenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThueNhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
